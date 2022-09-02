41e6dda2570725269ee9c686df5ce3a1fb7b3668
JoaoMartins-Forge <75683555+JoaoMartins-Forge@users.noreply.github.com>
2022-09-02 18:05:54 -0300

    first commit

---
+++ ExtractMaterials.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.Attributes;
+using DesignAutomationFramework;
+using Autodesk.Revit.ApplicationServices;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+
+namespace ExtractMaterialQuantities
+{
+	[Regeneration(RegenerationOption.Manual)]
+	[Transaction(TransactionMode.Manual)]
+	public class ExtractMaterials : IExternalDBApplication
+	{
+			public static void ExtractModelMaterials(DesignAutomationData data)
+			{
+
+				if (data == null) throw new ArgumentNullException(nameof(data));
+
+				Application rvtApp = data.RevitApp;
+				if (rvtApp == null) throw new InvalidDataException(nameof(rvtApp));
+
+				string modelPath = data.FilePath;
+				if (String.IsNullOrWhiteSpace(modelPath)) throw new InvalidDataException(nameof(modelPath));
+
+				Document doc = data.RevitDoc;
+				if (doc == null) throw new InvalidOperationException("Could not open document.");
+
+				dynamic urnResult = new JObject();
+
+				List<Element> elements = new FilteredElementCollector(doc).WhereElementIsNotElementType().ToList();
+
+				foreach (Element element in elements)
+				{
+					try
+					{
+						Category elementCategory = element.Category;
+
+						List<ElementId> materialsIds = element.GetMaterialIds(false).ToList();
+
+						foreach (ElementId materialId in materialsIds)
+						{
+							try
+							{
+								Element material = doc.GetElement(materialId);
+								double materialArea = element.GetMaterialArea(materialId, false);
+								double materialVolume = element.GetMaterialVolume(materialId);
+
+								dynamic newMaterial = new JObject();
+								newMaterial.externalId = element.UniqueId.ToString();
+								newMaterial.revitcategory = elementCategory.Name;
+								newMaterial.revitmaterial = material.Name;
+								newMaterial.materialareaqty = UnitUtils.ConvertFromInternalUnits(materialArea, UnitTypeId.SquareMeters);
+								newMaterial.materialareaqtytype = UnitTypeId.SquareMeters.TypeId;
+								newMaterial.materialvolumeqty = UnitUtils.ConvertFromInternalUnits(materialVolume, UnitTypeId.CubicMeters);
+								newMaterial.materialvolumeqtytype = UnitTypeId.CubicMeters.TypeId;
+
+								Parameter elementLength = element.LookupParameter("Length");
+								newMaterial.elementlength = UnitUtils.ConvertFromInternalUnits(elementLength != null ? elementLength.AsDouble() : 0, UnitTypeId.Meters);
+								newMaterial.elementlengthqtytype = UnitTypeId.Meters.TypeId;
+
+								urnResult.results.Add(newMaterial);
+							}
+							catch (Exception ex)
+							{
+								Console.WriteLine($"Error with material {materialId} from element {element.UniqueId}!");
+								Console.WriteLine(ex.Message);
+							}
+						}
+					}
+					catch (Exception ex)
+					{
+						Console.WriteLine($"Error with element {element.UniqueId}!");
+						Console.WriteLine(ex.Message);
+					}
+
+				}
+
+				// save all to a .json file
+				using (StreamWriter file = File.CreateText("result.json"))
+				using (JsonTextWriter writer = new JsonTextWriter(file))
+				{
+					urnResult.WriteTo(writer);
+				}
+
+			}
+
+			public ExternalDBApplicationResult OnShutdown(ControlledApplication application)
+			{
+				return ExternalDBApplicationResult.Succeeded;
+			}
+
+			public ExternalDBApplicationResult OnStartup(ControlledApplication application)
+			{
+				DesignAutomationBridge.DesignAutomationReadyEvent += HandleDesignAutomationReadyEvent;
+				return ExternalDBApplicationResult.Succeeded;
+			}
+
+			private void HandleDesignAutomationReadyEvent(object sender, DesignAutomationReadyEventArgs e)
+			{
+				e.Succeeded = true;
+				ExtractModelMaterials(e.DesignAutomationData);
+			}
+	}
+}
---
+++ README.md
@@ -0,0 +1,97 @@
+﻿# Revit CompoundStructureExtractor bundle for Forge Design Automation
+
+[![Design Automation](https://img.shields.io/badge/Design%20Automation-v3-green.svg)](http://developer.autodesk.com/)
+
+![Windows](https://img.shields.io/badge/Plugins-Windows-lightgrey.svg)
+![.NET](https://img.shields.io/badge/.NET%20Framework-4.8-blue.svg)
+[![Revit-2022](https://img.shields.io/badge/Revit-2022-lightgrey.svg)](http://autodesk.com/revit)
+
+
+# Description
+
+This sample demonstrates a way to retrieve quantities of materials for a Revit project based on a refference view,a mapping JSON file (to define the units for each material and proper ExportClassAs used by OneClick - just like class-mapping.json) and one JSON with parameters to define urn and viewname [Autodesk Revit IFC](https://github.com/Autodesk/revit-ifc)
+
+# Development Setup
+
+## Prerequisites
+
+1. **Forge Account**: Learn how to create a Forge Account, activate subscription and create an app at [this tutorial](http://learnforge.autodesk.io/#/account/). 
+2. **Visual Studio 2022** (Windows).
+3. **Revit 2022**: required to compile changes into the plugin
+
+## Design Automation Setup
+
+### Activity example
+
+```json
+{
+  "id": "RevitIfcExportorActivity",
+  "commandLine": [
+    "$(engine.path)\\\\revitcoreconsole.exe /i \"$(args[inputFile].path)\" /al \"$(appbundles[RevitIfcExportor].path)\""
+  ],
+  "parameters": {
+    "inputFile": {
+      "verb": "get",
+      "description": "Input Revit File",
+      "required": true,
+      "localName": "$(inputFile)"
+    },
+    "result": {
+      "zip": false,
+      "verb": "put",
+      "description": "Extracted materials from model",
+      "localName": "result.json"
+    }
+  },
+  "engine": "Autodesk.Revit+2022",
+  "appbundles": [
+    "Autodesk.MaterialsExtractor+dev"
+  ],
+  "description": "Activity of Revit Materials exporter"
+}
+```
+
+### Workitem example
+
+```json
+{
+  "activityId": "ID OF THE ACTIVITY",
+  "arguments": {
+    "inputFile": {
+      "url": "URL TO DOWNLOAD THE INPUT FILE (BIM360/ACC)",
+      "Headers": {
+        "Authorization": "Bearer TOKEN"
+      }
+    },
+    "result": {
+      "verb": "post",
+      "url": "URL TO UPLOAD THE RESULT",
+      "Headers": {
+        "Content-Type": "application/json"
+      }
+    },
+    "onProgress": {
+      "verb": "post",
+      "url": "URL TO RECEIVE PROGRESS INFORMATION",
+      "Headers": {
+        "Content-Type": "application/json"
+      }
+    },
+    "onComplete": {
+      "verb": "post",
+      "url": "URL TO RECEIVE STATUS AFTER JOB EXECUTION",
+      "Headers": {
+        "Content-Type": "application/json"
+      }
+    }
+  }
+}
+```
+
+## License
+
+This sample is licensed under the terms of the [MIT License](http://opensource.org/licenses/MIT). Please see the [LICENSE](LICENSE) file for full details.
+
+## Written by
+
+João Martins [@JooPaulodeOrne2](http://twitter.com/JooPaulodeOrne2), [Forge Partner Development](http://forge.autodesk.com)
